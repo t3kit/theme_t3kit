8a5844bb21743efad401b92f6ae34e28c3dd62a3
dmytro <dmytro@pixelant.se>
2015-10-30 15:33:37 +0200

    [CHORE] update all icons, add custom(css,js), update felayout

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,1 @@
 <?php
-
-// Custom CSS include
-if (TYPO3_MODE=="BE")   {
-    $TBE_STYLES['inDocStyles_TBEstyle'] .= '@import "/typo3conf/ext/theme_t3kit/Resources/Public/Css/Backend/be.css";';
-}
-
