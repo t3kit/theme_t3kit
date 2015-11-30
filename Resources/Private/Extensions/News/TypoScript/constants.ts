f6d54ce854fbf164b09cf990329e2d278fec98cf
dmytro <dmytro@pixelant.se>
2015-11-30 16:47:59 +0200

    [STYLE] update editorconfig, formatting

--- Resources/Private/Extensions/News/TypoScript/constants.ts
+++ Resources/Private/Extensions/News/TypoScript/constants.ts
@@ -1,13 +1,13 @@
 plugin.tx_news {
-  view {
-    # cat=plugin.tx_news/file; type=string; label=Path to template root (FE)
-    templateRootPath = EXT:theme_t3kit/Resources/Private/Extensions/News/Templates/
-    # cat=plugin.tx_news/file; type=string; label=Path to template partials (FE)
-    partialRootPath = EXT:theme_t3kit/Resources/Private/Extensions/News/Partials/
-  }
+    view {
+        # cat=plugin.tx_news/file; type=string; label=Path to template root (FE)
+        templateRootPath = EXT:theme_t3kit/Resources/Private/Extensions/News/Templates/
+        # cat=plugin.tx_news/file; type=string; label=Path to template partials (FE)
+        partialRootPath = EXT:theme_t3kit/Resources/Private/Extensions/News/Partials/
+    }
 
-  settings {
-    # cat=plugin.tx_news/file; type=string; label=Path to CSS file
-    cssFile =
-  }
+    settings {
+        # cat=plugin.tx_news/file; type=string; label=Path to CSS file
+        cssFile =
+    }
 }
