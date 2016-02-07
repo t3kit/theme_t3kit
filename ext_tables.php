d023a6bd2f607f1ca58b646a452574ebd0b8450a
dmytro <dmytro@pixelant.se>
2016-02-07 17:29:48 +0200

    update config for Custom Content Elements

--- ext_tables.php
+++ ext_tables.php
@@ -46,23 +46,29 @@ if (TYPO3_MODE === 'BE') {
         \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         ['source' => 'EXT:theme_t3kit/Resources/Public/Icons/ContentElements/tabs.svg']
     );
-    # PageTS custom content elements
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        'theme_t3kit',
-        'Configuration/PageTS/custom_content_elements.txt',
-        'EXT:theme_t3kit :: Enable Custom Content Elemets'
-    );
-    # Static files for custom content elements
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Configuration/TypoScript/CustomContentElements/',
-        'EXT:theme_t3kit :: Enable Custom Content Elemets'
-    );
+
     // Add context sensitive help (csh) for the haiku table
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
         'tt_content',
         'EXT:theme_t3kit/Resources/Private/Language/locallang_csh_tt_content.xml'
     );
+
+    /***************
+     * Custom content elements
+     */
+
+    // # PageTS custom content elements
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    //     $_EXTKEY,
+    //     'Configuration/PageTS/',
+    //     'EXT:theme_t3kit :: Enable Custom Content Elemets'
+    // );
+    // # Static files for custom content elements
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    //     $_EXTKEY,
+    //     'Configuration/TypoScript/CustomContentElements/',
+    //     'EXT:theme_t3kit :: Enable Custom Content Elemets'
+    // );
     // Include ext_tables from 'custom_content_elements'
     if (is_file(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php')) {
         require_once(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php');
