ce1740c3744c80c9a41aa9a6fb2048ac56d0229c
Mats Svensson <mats@pixelant.se>
2016-02-26 14:42:04 +0100

    added setup ts for seo_basics

---
+++ Resources/Private/Extensions/seo_basics/TypoScript/setup.ts
@@ -0,0 +1,12 @@
+tx_seo_xmlsitemaps.10.sysLanguageHrefLangMappings {
+    # sys_language_uid = hreflang
+    0 = en
+    1 = sv
+    2 = de
+    3 = ro
+    4 = uk
+}
+#
+# Limit allowed doktypes to only Standard pages
+# (only affects page selection if set)
+tx_seo_xmlsitemaps.10.allowedDoktypes = 1
