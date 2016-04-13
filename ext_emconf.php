ee169af2e19d5eb8d8f2ef7d7a687acf59e78689
dmytro <dmytro@pixelant.se>
2016-04-13 16:50:52 +0300

    [DOC] update ext info

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Theme t3kit',
-	'description' => 'Theme t3kit',
+	'description' => 'Base TYPO3 Theme fot t3kit project',
 	'category' => 'templates',
 	'shy' => 0,
 	'version' => '1.0.0',
