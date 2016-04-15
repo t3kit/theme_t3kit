bf6601bb46a24a6e1cd973d91c32f3bb54a1d3df
root <root@li351-66.members.linode.com>
2016-04-15 14:55:38 +0200

    [FIX] headers for CE, added properly copy/localize actions

--- Classes/Hooks/DataHandler.php
+++ Classes/Hooks/DataHandler.php
@@ -6,8 +6,6 @@ use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility as gu;
 use TYPO3\CMS\Backend\Utility\BackendUtility as bu;
 
-//use TYPO3\CMS\Extbase\Utility\DebuggerUtility as du;
-
 /**
  * Class/Function which offers TCE main hook functions.
  *
@@ -23,7 +21,8 @@ class DataHandler implements SingletonInterface {
 
 	$getArray = gu::_GET();
 	// we need only action 'copyFromLanguage'
-	if ($getArray['action'] == "copyFromLanguage"){
+	if ($getArray['action'] == "copyFromLanguage" ||
+		($getArray['action'] == "localize" && $getArray['srcLanguageId'] > 0)){
 
 	    if (count($getArray['uidList']) > 0){
 		// create preTitle like [Translate to English:]
@@ -38,18 +37,21 @@ class DataHandler implements SingletonInterface {
 		    if (empty($localizedCeUid)){
 			continue;
 		    }
-		    $this->localizeChildrenRecordsForGridElement($origCeUid, $localizedCeUid,
-				$tcemainObj->copyMappingArray_merged['tt_content'],
-				$getArray['srcLanguageId'], $preTitle);
-
+		    $this->localizeChildrenRecordsForGridElement($getArray['action'],
+			    $origCeUid,
+			    $localizedCeUid,
+			    $tcemainObj->copyMappingArray_merged['tt_content'],
+			    $getArray['srcLanguageId'],
+			    $preTitle);
 		}
 	    }
-
 	}
     }
 
     /**
+     * localize children records for grid element and fix headers for other CE
      *
+     * @param string	$action - name of action, like 'localize' or 'copyFromLanguage'
      * @param integer	$origCeUid - uid of original CE
      * @param integer	$localizedCeUid - uid of localized CE
      * @param array	$localizeMapping - array with relations between CE original --> localized
@@ -58,11 +60,16 @@ class DataHandler implements SingletonInterface {
      *
      * @return void
      */
-    private function localizeChildrenRecordsForGridElement ($origCeUid, $localizedCeUid, $localizeMapping, $sourceLanguageUid, $preTitle){
+    private function localizeChildrenRecordsForGridElement (
+	    $action,
+	    $origCeUid,
+	    $localizedCeUid,
+	    $localizeMapping,
+	    $sourceLanguageUid,
+	    $preTitle){
 
 	$localizedRecord = bu::getRecord('tt_content', $localizedCeUid,
 		'uid,pid,Ctype,sys_language_uid,tx_gridelements_children,tx_gridelements_container,l18n_parent,header');
-
 
 	// check if this record is gridelement and has related records (childrens)
 	if ($localizedRecord['Ctype'] == "gridelements_pi1" && $localizedRecord['tx_gridelements_children'] > 0){
@@ -78,32 +85,53 @@ class DataHandler implements SingletonInterface {
 			continue;
 		    }
 		    // define l18n_parent for children records
-		    if ($sourceLanguageUid > 0){
-			$tmpRecord = bu::getRecord('tt_content', $record['uid'], 'l18n_parent');
-			$l18nparent = $tmpRecord['l18n_parent'];
-		    } else {
-			$l18nparent = $key;
+		    $l18nparent = 0;
+		    if ($action == 'localize'){
+			if ($sourceLanguageUid > 0){
+			    $tmpRecord = bu::getRecord('tt_content', $record['uid'], 'l18n_parent');
+			    $l18nparent = $tmpRecord['l18n_parent'];
+			} else {
+			    $l18nparent = $key;
+			}
 		    }
+
 		    // update children records
-		    $GLOBALS['TYPO3_DB']->sql_query('UPDATE tt_content SET sys_language_uid = '. $localizedRecord['sys_language_uid'].
-			    ', l18n_parent = '. $l18nparent .', header = \''. $preTitle . $record['header'] . '\' WHERE uid = '. $record['uid']
+		    $GLOBALS['TYPO3_DB']->sql_query('UPDATE tt_content '
+			    . 'SET sys_language_uid = '. $localizedRecord['sys_language_uid']. ', t3_origuid = '. $key
+			    . ', l18n_parent = '. $l18nparent
+			    . ' WHERE uid = '. $record['uid']
 			);
 
-		    $this->localizeChildrenRecordsForGridElement($key, $record['uid'], $localizeMapping, $sourceLanguageUid, $preTitle);
+		    $this->localizeChildrenRecordsForGridElement($action,
+			    $key,
+			    $record['uid'],
+			    $localizeMapping,
+			    $sourceLanguageUid,
+			    $preTitle);
 		}
 	    }
 	    // define l18n_parent for grid container record
-	    if ($sourceLanguageUid > 0){
-		$tmpRecord = bu::getRecord('tt_content', $origCeUid, 'l18n_parent');
-		$l18nparent = $tmpRecord['l18n_parent'];
-	    } else {
-		$l18nparent = $origCeUid;
+	    $l18nparent = 0;
+	    if ($action == 'localize'){
+		if ($sourceLanguageUid > 0){
+		    $tmpRecord = bu::getRecord('tt_content', $origCeUid, 'l18n_parent');
+		    $l18nparent = $tmpRecord['l18n_parent'];
+		} else {
+		    $l18nparent = $origCeUid;
+		}
 	    }
 	    // update grid container record
-	    $GLOBALS['TYPO3_DB']->sql_query('UPDATE tt_content SET l18n_parent = '. $l18nparent .
-		    ', header = \''. $preTitle . $localizedRecord['header'] . '\' WHERE uid = '. $localizedRecord['uid']
+	    $GLOBALS['TYPO3_DB']->sql_query('UPDATE tt_content SET '
+		    . 'l18n_parent = '. $l18nparent . ', t3_origuid = '. $origCeUid
+		    . ', header = \''. $preTitle . $localizedRecord['header'] . '\''
+		    . ' WHERE uid = '. $localizedRecord['uid']
 		);
 
+	} else {
+	    // just update header for all other CE and empty grid
+	    $GLOBALS['TYPO3_DB']->sql_query('UPDATE tt_content SET header = "'
+		    . $preTitle . $localizedRecord['header']
+		    . '" WHERE uid = '. $localizedRecord['uid']);
 	}
 
     }
