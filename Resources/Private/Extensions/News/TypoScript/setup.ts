cc298a83abe129e6768d903fcd29bc5f39ae86b1
Anjey <andriy@pixelant.se>
2016-05-31 14:12:22 +0300

    [FIX] fixed light box class name for news detail view

--- Resources/Private/Extensions/News/TypoScript/setup.ts
+++ Resources/Private/Extensions/News/TypoScript/setup.ts
@@ -1,1 +1,2 @@
 plugin.tx_news.settings.defaultDetailPid = {$themes.configuration.features.newsDefaultDetailPid}
+plugin.tx_news.settings.detail.media.image.lightbox.class = {$styles.content.textmedia.linkWrap.lightboxCssClass}
