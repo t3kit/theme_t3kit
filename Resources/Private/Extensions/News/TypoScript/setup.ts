1ff3429b3e112b4b067b455254516f7bce42da9b
dmh <dmytro.hrynevych@gmail.com>
2016-07-01 19:47:21 +0300

    update news carousel cropMaxCharacters

--- Resources/Private/Extensions/News/TypoScript/setup.ts
+++ Resources/Private/Extensions/News/TypoScript/setup.ts
@@ -1,4 +1,4 @@
 plugin.tx_news.settings.defaultDetailPid = {$themes.configuration.features.newsDefaultDetailPid}
 plugin.tx_news.settings.detail.media.image.lightbox.class = {$styles.content.textmedia.linkWrap.lightboxCssClass}
 
-plugin.tx_news.settings.newsCarousel.cropMaxCharacters = 120
+plugin.tx_news.settings.newsCarousel.cropMaxCharacters = 136
