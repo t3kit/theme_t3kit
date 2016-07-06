756bb8385e6925d578c3e4d955ec7d108c7a0f2f
dmh <dmytro.hrynevych@gmail.com>
2016-07-06 11:35:02 +0300

    [FEATURE] add new list templates for news EXT, (cards, simple list)

--- Resources/Private/Extensions/News/TypoScript/setup.ts
+++ Resources/Private/Extensions/News/TypoScript/setup.ts
@@ -2,3 +2,7 @@ plugin.tx_news.settings.defaultDetailPid = {$themes.configuration.features.newsD
 plugin.tx_news.settings.detail.media.image.lightbox.class = {$styles.content.textmedia.linkWrap.lightboxCssClass}
 
 plugin.tx_news.settings.newsCarousel.cropMaxCharacters = 136
+
+plugin.tx_news.settings.card.cropMaxCharacters = 200
+
+plugin.tx_news.settings.simpleList.cropMaxCharacters = 280
