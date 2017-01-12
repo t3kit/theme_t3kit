46b519ed1b3f07a9f6718f45f2f2e3f3a8115b7b
myroslav321 <myroslavlutsiv@gmail.com>
2017-01-12 16:00:53 +0200

    [FEATURE] changed default news dummyImage to the new one (Podio feature_60) (#110)

--- Resources/Private/Extensions/News/TypoScript/setup.ts
+++ Resources/Private/Extensions/News/TypoScript/setup.ts
@@ -8,3 +8,5 @@ plugin.tx_news.settings.card.cropMaxCharacters = 200
 plugin.tx_news.settings.simpleList.cropMaxCharacters = 280
 
 plugin.tx_news.settings.timeline.cropMaxCharacters = 500
+
+plugin.tx_news.settings.list.media.dummyImage = typo3conf/ext/theme_t3kit/Resources/Public/Extensions/News/images/no_image.png
