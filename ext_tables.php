655fe158608f0b4c1282992c6d3f2e28486cf654
Andriy Oprysko <andriy@pixelant.se>
2017-01-18 14:29:18 +0200

    [BUGFIX] broken icons of grid elements in typo 8.5

--- ext_tables.php
+++ ext_tables.php
@@ -1,86 +1,38 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-$contentElementIconFilePrefix = 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ContentElements/';
+$boot = function ($_EXTKEY) {
+    if (TYPO3_MODE === 'BE') {
+        $contentElementIconFilePrefix = 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ContentElements/';
 
-if (TYPO3_MODE === 'BE') {
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'content-elements-accordion',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'accordion.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-contentElementSlider',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'slider.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-bigIconTextButton',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'bigIconTextButton.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-iconTextButton',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'iconTextButton.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-imageTextLink',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'imageTextLink.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-logoCarousel',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'logoCarousel.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-quote',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'quote.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-tabs',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'tabs.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-fullWidthImage',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'fullWidthImage.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-responsiveVideo',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'responsiveVideo.svg']
-    );
+        $contentElementIcons = [
+            'content-elements-accordion' => 'accordion.svg',
+            'content-elements-contentElementSlider' => 'slider.svg',
+            'content-elements-bigIconTextButton' => 'bigIconTextButton.svg',
+            'content-elements-iconTextButton' => 'iconTextButton.svg',
+            'content-elements-imageTextLink' => 'imageTextLink.svg',
+            'content-elements-logoCarousel' => 'logoCarousel.svg',
+            'content-elements-quote' => 'quote.svg',
+            'content-elements-tabs' => 'tabs.svg',
+            'content-elements-fullWidthImage' => 'fullWidthImage.svg',
+            'content-elements-responsiveVideo' => 'responsiveVideo.svg',
+            'content-elements-socialIcons' => 'socialIcons.svg',
+            'content-elements-copyrightText' => 'copyrightText.svg',
+            'content-elements-contacts' => 'contacts.svg',
+            'content-center-vertical-text-img-left' => 'content-center-vertical-text-img-left.svg',
+            'content-center-vertical-text-img-right' => 'content-center-vertical-text-img-right.svg',
+        ];
 
-    $iconRegistry->registerIcon(
-        'content-elements-heroImage',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'fullWidthImage.svg']
-    );
+        /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
-
-    $iconRegistry->registerIcon(
-        'content-elements-socialIcons',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'socialIcons.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-copyrightText',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'copyrightText.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-elements-contacts',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'contacts.svg']
-    );
+        foreach ($contentElementIcons as $identifier => $contentElementIcon) {
+            $iconRegistry->registerIcon(
+                $identifier,
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => $contentElementIconFilePrefix . $contentElementIcon]
+            );
+        }
 
 	$iconRegistry->registerIcon(
 		'guide-tour-themes-module',
@@ -90,42 +42,61 @@ if (TYPO3_MODE === 'BE') {
 		)
 	);
 
-    $iconRegistry->registerIcon(
-        'content-center-vertical-text-img-left',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'content-center-vertical-text-img-left.svg']
-    );
-    $iconRegistry->registerIcon(
-        'content-center-vertical-text-img-right',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => $contentElementIconFilePrefix . 'content-center-vertical-text-img-right.svg']
-    );
 
-    // Add context sensitive help (csh) for the haiku table
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-        'tt_content',
-        'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tt_content.xml'
-    );
+        // Grid element icons
+        $gridElementsPath = 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/GridElements/';
 
-    /***************
-     * Custom content elements
-     */
-    # PageTS custom content elements
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        $_EXTKEY,
-        'Configuration/PageTS/custom_content_elements.txt',
-        'EXT:theme_t3kit :: Enable Custom Content Elemets (fileadmin/templates/...)'
-    );
-    # Static files for custom content elements
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Configuration/TypoScript/CustomContentElements/',
-        'EXT:theme_t3kit :: Enable Custom Content Elemets (fileadmin/templates/...)'
-    );
+        $gridElementsIcons = [
+            'grid-elements-2ColumnGrid' => '2-column-grid.svg',
+            'grid-elements-3ColumnGrid' => '3-column-grid.svg',
+            'grid-elements-4ColumnGrid' => '4-column-grid.svg',
+            'grid-elements-adv1ColumnGrid' => 'adv1-column-grid.svg',
+            'grid-elements-adv2ColumnGrid' => 'adv2-column-grid.svg',
+            'grid-elements-adv3ColumnGrid' => 'adv3-column-grid.svg',
+            'grid-elements-adv4ColumnGrid' => 'adv4-column-grid.svg',
+            'grid-elements-collapsible' => 'collapsible.svg',
+            'grid-elements-collapsibleGroup' => 'collapsibleGroup.svg',
+            'grid-elements-parallax' => 'parallax.svg',
+            'grid-elements-tabGroup' => 'tabGroup.svg',
+            'grid-elements-tab' => 'tab.svg'
+        ];
 
-    // Include ext_tables from 'custom_content_elements'
-    if (is_file(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php')) {
-        require_once(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php');
+        foreach ($gridElementsIcons as $identifier => $gridElementsIcon) {
+            $iconRegistry->registerIcon(
+                $identifier,
+                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                ['source' => $gridElementsPath . $gridElementsIcon]
+            );
+        }
+
+        // Add context sensitive help (csh) for the haiku table
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tt_content',
+            'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tt_content.xml'
+        );
+
+        /***************
+         * Custom content elements
+         */
+        # PageTS custom content elements
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+            $_EXTKEY,
+            'Configuration/PageTS/custom_content_elements.txt',
+            'EXT:theme_t3kit :: Enable Custom Content Elemets (fileadmin/templates/...)'
+        );
+        # Static files for custom content elements
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Configuration/TypoScript/CustomContentElements/',
+            'EXT:theme_t3kit :: Enable Custom Content Elemets (fileadmin/templates/...)'
+        );
+
+        // Include ext_tables from 'custom_content_elements'
+        if (is_file(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php')) {
+            require_once(PATH_site . 'fileadmin/templates/theme_t3kit/custom_content_elements/Configuration/Backend/ext_tables.php');
+        }
+
     }
-
-}
+};
+$boot($_EXTKEY);
+unset($boot);
