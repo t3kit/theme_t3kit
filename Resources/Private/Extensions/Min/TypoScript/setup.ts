955232bfc775bb79bfdc388648bdc4f14d89b448
Kakada Neang <kakada@web-essentials.asia>
2017-02-14 11:29:53 +0700

    [FEATURE] a lot of whitespace in markup output (Podio feature_47)

---
+++ Resources/Private/Extensions/Min/TypoScript/setup.ts
@@ -0,0 +1,18 @@
+plugin.tx_min.tinysource {
+	enable = 1
+	head {
+		stripTabs = 0
+		stripNewLines = 0
+		stripDoubleSpaces = 1
+		stripTwoLinesToOne = 1
+	}
+	body {
+		stripComments = 0
+		stripTabs = 1
+		stripNewLines = 0
+		stripDoubleSpaces = 1
+		stripTwoLinesToOne = 1
+		preventStripOfSearchComment = 1
+	}
+	oneLineMode = 1
+}
