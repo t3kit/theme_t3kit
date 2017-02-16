0936b616573e2ee1fa8d7ee44a97d56c707eae57
dmh <dmytro.hrynevych@gmail.com>
2017-02-16 17:43:06 +0200

    [TASK] improve guide ThemeModule, add more step, change pageTS folder

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,5 +9,5 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 	// Add page typoscript tours
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/Guides/tsconfig.txt">');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/Extensions/Guides/PageTS/tsconfig.txt">');
 }
