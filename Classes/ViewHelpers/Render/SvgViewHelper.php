ad90dae48311b97d271653128e2f803621fe6107
Mats Svensson <mats@pixelant.se>
2017-04-07 12:54:54 +0200

    [TASK] add svg viewhelper to theme_t3kit and register t3kit fluid namespaces

---
+++ Classes/ViewHelpers/Render/SvgViewHelper.php
@@ -0,0 +1,110 @@
+<?php
+namespace T3kit\themeT3kit\ViewHelpers\Render;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2016 Andrii Pozdieiev <andriy.p@pixelant.se>, Mosa Al-Husseini <mosa@pixelant.se>, Pixelant AB
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+class SvgViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+    /**
+     * @var boolean
+     */
+    protected $escapeChildren = false;
+
+    /**
+     * @var boolean
+     */
+    protected $escapeOutput = false;
+
+
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('class', 'string', 'Specifies an alternate class for the svg', false);
+        $this->registerArgument('width', 'float', 'Specifies a width for the svg', false);
+        $this->registerArgument('height', 'float', 'Specifies a height for the svg', false);
+
+    }
+
+    /**
+     * Generate a list from the content
+     *
+     * @param string $source
+     */
+    public function render($source)
+    {
+        $sourceAbs = PATH_site.$source;
+
+        /*
+        *   Todo: add absoulte path for source
+        *
+        $source = 'typo3conf/ext/theme_t3kit/Resources/Private/Templates/Theme/Content.html';
+
+        if (\TYPO3\CMS\Core\Utility\GeneralUtility::isAllowedAbsPath($source)) {
+          return 'unable to open file';
+        }
+
+        $sourceAbs = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($source);
+        *
+        */
+        if (!file_exists($sourceAbs)) {
+            return 'no SVG file on /'.$source;
+        }
+        return $this->getInlineSvg($sourceAbs);
+    }
+
+    /**
+     * @param string $source
+     *
+     * @return string
+     */
+    protected function getInlineSvg($source)
+    {
+
+        if (!file_exists($source)) {
+            return '';
+        }
+
+        $svgContent = file_get_contents($source);
+        $svgContent = preg_replace('/<script[\s\S]*?>[\s\S]*?<\/script>/i', '', $svgContent);
+        // Disables the functionality to allow external entities to be loaded when parsing the XML, must be kept
+        $previousValueOfEntityLoader = libxml_disable_entity_loader(true);
+        $svgElement = simplexml_load_string($svgContent);
+        libxml_disable_entity_loader($previousValueOfEntityLoader);
+
+        // remove xml version tag
+        $domXml = dom_import_simplexml($svgElement);
+        if (isset($this->arguments['class'])) {
+            $domXml->setAttribute('class', $this->arguments['class']);
+        }
+        if (isset($this->arguments['width'])) {
+            $domXml->setAttribute('width', $this->arguments['width']);
+        }
+        if (isset($this->arguments['height'])) {
+            $domXml->setAttribute('height', $this->arguments['height']);
+        }
+        return $domXml->ownerDocument->saveXML($domXml->ownerDocument->documentElement);
+    }
+}
