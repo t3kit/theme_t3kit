6c9611d339cc3f121e3fbd262e9a95f1220d81dd
dmh <dmytro.hrynevych@gmail.com>
2017-04-18 18:51:41 +0300

    [TASK] add conf for CKEditor, remove Rtehtmlarea

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    'setup.default.rteCleanPasteBehaviour=pasteStructure'
-);
-
 $GLOBALS['TYPO3_CONF_VARS']
     ['SC_OPTIONS']
     ['cms/layout/class.tx_cms_layout.php']
@@ -50,3 +46,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php'][
 	= \T3kit\themeT3kit\Hooks\ImageTextLinkPreviewRenderer::class;
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['t3kit'][] = 'T3kit\\themeT3kit\\ViewHelpers';
+
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['t3kit_default'] = 'EXT:theme_t3kit/Resources/Private/Extensions/RTE/Default.yaml';
