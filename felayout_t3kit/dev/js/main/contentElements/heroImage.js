bc2dd806c8849c03a6685dd466c2321704807517
Myroslav <myroslavlutsiv@gmail.com>
2017-05-10 10:00:00 +0300

    [FIX] Hero Image, fix mobile view

--- felayout_t3kit/dev/js/main/contentElements/heroImage.js
+++ felayout_t3kit/dev/js/main/contentElements/heroImage.js
@@ -9,6 +9,13 @@
     var $slider = $('.slider-container')
     $frame.each(function () {
       var self = $(this)
+      var $p = self.find('.hero-image__caption-p')
+      if ($p.length) {
+        $p.dotdotdot({
+          watch: 'window',
+          height: 55
+        })
+      }
       if (!self.parents('.swiper-wrapper').length) {
         self.addClass('_animated')
       }
