bbc9342db2285f1d5ea0d11bbd005de1ca19bb32
Mikkel Hastrup <mikkel@pixelant.se>
2018-06-14 16:22:14 +0200

    [TASK] removal of console.log

--- Resources/Public/less/main.js
+++ Resources/Public/less/main.js
@@ -287,8 +287,6 @@ jQuery(function ($) {
         disableParallax: /iPad|iPhone|iPod|Edge/ // disable Ios and Microsoft Edge
       })
       $('.parallax-resimg').each(function () {
-        console.log($(this).css('background-image'))
-        console.log($(this).css('background-image').match(/\(([^)]+)\)/)[1].replace(/"/g, ''))
         $(this).jarallax({
           type: 'scroll', // scroll, scale, opacity, scroll-opacity, scale-opacity
           speed: 0.5,
@@ -297,7 +295,6 @@ jQuery(function ($) {
         })
       })
       $('.parallax-video').each(function () {
-        console.log($(this).attr('data-video-url'))
         $(this).jarallax({
           type: 'scroll', // scroll, scale, opacity, scroll-opacity, scale-opacity
           speed: 0.5,
