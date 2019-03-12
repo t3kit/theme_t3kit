373a3bcc6898b9d6bedf63fbe26c1c715a888871
Mosa <mosaalhusseini@hotmail.com>
2019-03-12 14:25:26 +0100

    [BUGFIX] fixes slider container effects (#489)

--- felayout_t3kit/dev/js/main/contentElements/sliderContainer.js
+++ felayout_t3kit/dev/js/main/contentElements/sliderContainer.js
@@ -44,7 +44,7 @@
           widthForMediumLaptop = 3
         }
         var slider = new Swiper(currentSlider, {
-          containerModifierClass: 'js__slider-container__container',
+          containerModifierClass: 'swiper-container-',
           wrapperClass: 'js__slider-container__wrapper',
           slideClass: 'js__slider-container__slide',
           nextButton: currentSlider.parent().find('.js__slider-container__btn-next'),
