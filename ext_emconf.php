b282b9298eb8ae0561e3219dff761ce6612913a9
dmh <dmytro.hrynevych@gmail.com>
2019-05-27 16:23:46 +0300

    [BUGFIX] fix the order of required extensions

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,11 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3'  => '9.5.0-9.5.99',
-            'news' => '7.1.0-7.9.99'
+            'gridelements' => '9.2.1-9.2.99',
+            'dyncss_less' => '0.8.0-0.8.99',
+            'themes' => '8.7.6-8.7.99',
+            'news' => '7.1.0-7.9.99',
+            'frontend_editing' => '1.4.6-1.9.99'
         ),
         'conflicts' => array(
         ),
